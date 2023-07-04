104067ef88d6e257f10519a03b6bebe827d18367
Eugene <rejgan318@gmail.com>
2023-07-04 14:05:31 +0300

    config file parameters

--- color_ranges.py
+++ color_ranges.py
@@ -1,5 +1,16 @@
 """
 Определение закрашенных областей
 """
+import configparser
+
+from PIL import Image
+
+CONFIG_FILE = "config.ini"
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+
+IMG_DIR = config["DEFAULT"]["img_dir"]
+MAX_SAMPLE_SIZE = int(config["DEFAULT"]["max_sample_size"])
+
 
 print("Done.")
