d5f97ca1677b335622aac54ec4c6a1d10201970a
Eugene <rejgan318@gmail.com>
2023-07-05 20:50:15 +0300

    Заливка работает в первом приближении

--- color_ranges.py
+++ color_ranges.py
@@ -6,9 +6,10 @@ from pathlib import Path
 
 import colorama as co
 from PIL import Image
-import numpy as np
 
-from print_ascii import make_ascii_picture, total_colors, get_background_color, pack_rgb, back_rgb
+from print_ascii import make_ascii_picture, total_colors, get_background_color, get_color_from_pixel, pack_rgb, back_rgb
+
+pos = lambda y, x: co.Cursor.POS(x, y)
 
 print(f"{__file__}")
 
@@ -26,12 +27,70 @@ img_name = config["DEFAULT"]["img_name"]
 assert (Path(IMG_DIR) / img_name).exists(), f"Файл {img_name} не найден"
 
 co.just_fix_windows_console()
+print(co.ansi.clear_screen() + pos(1, 1))
 
 img = Image.open(Path(IMG_DIR) / img_name)
-colors = total_colors(img)
 print(make_ascii_picture(img))
-bg_color = get_background_color(img)
+colors = total_colors(img)
 print(f"Всего цветов : {len(colors)} " + ''.join([back_rgb(*bg) + "  " for bg in colors]) + co.Back.RESET)
+bg_color = get_background_color(img)
 print(f"Цвет фона    : {back_rgb(*bg_color)}  {co.Back.RESET} "
       f"{bg_color}, #{bg_color[0]:02x}{bg_color[1]:02x}{bg_color[2]:02x}, {pack_rgb(bg_color)}")
 
+
+def print_char_xy(x: int, y: int, char: str, ascii: str):
+    print(pos(x + 1, y * 2 - 1) + ascii + char, end="")
+
+
+def check(x: int, y: int) -> bool:
+    if x < 0 or x > img.width - 1 or y < 0 or y > img.height - 1:
+        return False
+    if (x, y) in completed:
+        return False
+
+    completed.append((x, y))
+    if get_color_from_pixel(img, (x, y)) == bg_color:
+        return False
+    return True
+
+def check_around(x: int, y: int) -> bool:
+    if check(x , y):
+        stack.append((x, y))
+    else:
+        completed.append((x, y))
+
+pixels_count = img.width * img.height
+pixel_processed = 1
+regions = {}
+region_index = 0
+completed = []
+x = y = 0
+stack = []
+while len(completed) < pixels_count:
+    cur_color = get_color_from_pixel(img, (x, y))   # ищем первую и следующую область
+    if cur_color != bg_color and (x, y) not in completed:
+        region_index += 1     # start with 1?
+        regions[region_index] = []  # сюда мы будем сохранять координаты точек в каждой области
+        stack.append((x, y))
+    else:
+        completed.append((x, y))
+    while stack:    # Область найдена, заливаем
+        c, r = stack.pop()
+        regions[region_index].append((c, r))
+        completed.append((c, r))
+        print_char_xy(r + 1, c + 1, "++", "")
+
+        check_around(c - 1, r)
+        check_around(c + 1, r)
+        check_around(c, r - 1)
+        check_around(c, r + 1)
+
+    # print_char_xy(y + 1, x + 1, "00", "")
+    x += 1
+    if x == img.width:
+        x = 0
+        y += 1
+
+
+print(pos(20, 1))
+print("Done.")
